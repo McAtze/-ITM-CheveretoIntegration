7abd6d64f18318b2478d2e334058a1dddc802d64
McAtze <info@it-maku.com>
2018-05-31 19:15:30 +0200

    Create v2.0.0

---
+++ upload/src/addons/ITM/ChevInt/Setup.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ITM\ChevInt;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+}
